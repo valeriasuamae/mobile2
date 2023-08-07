653d7927e354e7816bf584e2ecc5d0c64a4da5d0
valeria <126163704+valeriasuamae@users.noreply.github.com>
2023-08-07 10:48:08 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,30 @@
+alert("estou aprendendo javascript");
+window.alert("BOM DIA");
+var x=Number(window.prompt("entre com um valor x:"));
+var y=Number(window.prompt("entre com um valor y:"));
+document.write ("<br> x+y=",x+y);
+document.write ("<br> x-y=",x-y);
+document.write ("<br> x/y=",x/y); 
+document.write ("<br> x*y=",x*y);
+document.write ("<br> x==y=",x==y);
+document.write ("<br> x>y?->",x>y);
+document.write ("<br> x<y?->",x<y);
+
+var x=5,
+    y=2,result;
+result=x/y;
+document.write("<br>",result);
+
+var x=5,
+    y=2,result;
+result=x%y;
+document.write("<br>",result);
+
+var nome=prompt("digite seu nome:", nome);
+document.write("<br>",nome)
+
+result=x++;
+result=x;
+result=++x;
+result=x+1;
+document.write(result);
